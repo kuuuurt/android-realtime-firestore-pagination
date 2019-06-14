de4448abf827c9ff872c0122688caec89740ea86
Kurt Renzo Acosta <kurt.incite@gmail.com>
2019-06-14 10:15:56 +0800

    Initial commit

---
+++ app/src/main/java/com/kurt/example/androidpaginationsample/presentation/records/RecordsActivity.kt
@@ -0,0 +1,18 @@
+package com.kurt.example.androidpaginationsample.presentation.records
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kurt.example.androidpaginationsample.R
+
+/**
+ * Copyright 2019, Kurt Renzo Acosta, All rights reserved.
+ *
+ * @author Kurt Renzo Acosta
+ * @since 06/14/2019
+ */
+class RecordsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_records)
+    }
+}
